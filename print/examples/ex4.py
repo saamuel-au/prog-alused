bc5bd810423224a887d393a179a6536fd6eabb8e
saamuel <saamuel.auvaart@voco.ee>
2023-10-09 13:59:07 +0300

    input

---
+++ print/examples/ex4.py
@@ -0,0 +1,7 @@
+name = "Saamuel"
+print(f"hi {name}!")  #Hi Saamuel!
+price_without_tax = 20
+print(f"price with tax is {price_without_tax * 1.2}")
+# Price with tax is 24.0
+print("Sum: {1 + 2 + 3}")   # Sum: {1 + 2 + 3}
+print(f"Sum: {1 + 2 + 3}")   #sum : 6
