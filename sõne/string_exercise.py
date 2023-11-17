5d8af7fa133adec4cca5375d03ca58d4d46803c4
saamuel <saamuel.auvaart@voco.ee>
2023-11-17 13:26:45 +0200

    string_exercise

---
+++ sõne/string_exercise.py
@@ -0,0 +1,21 @@
+first_name = "James"
+last_name = "Bond"
+
+
+full_name = F"{first_name} {last_name}"
+
+
+self_description_sentence = (f"My name is {last_name}, {full_name}.")
+
+
+cake = "vahukoormarjadtäidispõhi"
+print (cake.replace("vahukoor", "vahukoor\n").replace("marjad", "marjad\n").replace("täidis", "täidis\n"))
+
+
+original_string = "Programming is fun!"
+backwards =  original_string[: :-1]
+every_other = original_string[::2]
+first_word_reversed = original_string[10: :-1]
+
+
+
